d418e00c1b15ddb1516739f8cb7b4a9fb782e031
Kausik Dey <kausik008@gmail.com>
2018-07-15 21:45:23 +0530

    animation of vault added

--- vocab-vault/src/app/vault/vault.component.ts
+++ vocab-vault/src/app/vault/vault.component.ts
@@ -1,10 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 /**
- * image info : 
- * unlock-1 : 
- *  start :: 
- *  end :: image no - 138
+ * image info : 45 images in each animation 
  */
 
 @Component({
@@ -13,10 +10,57 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./vault.component.css']
 })
 export class VaultComponent implements OnInit {
+  groupIndex: number;
+  imgName: string;
+  intvID: any;
+  basePath: string;
+  imgGroup: Array<string>[];
 
   constructor() { }
 
   ngOnInit() {
+    this.groupIndex = 0;
+    this.imgName = 'lock0001.png';
+    this.basePath = "/assets/img/vault/";
+    this.imgGroup = []; 
+    this.createImgGroups();
+  }
+
+  createImgGroups(): void {
+    let imgNames = [];
+    for (let i = 1; i < 457; i++) {
+      imgNames.push(this.nextImage(i-1));
+      if (i % 46 === 0) {
+        this.imgGroup.push(imgNames);
+        imgNames = [];
+      }
+    }
+    this.imgGroup.push(imgNames);
+  }
+
+  unlockNext() {
+    let intv = 50;
+    let index = 0;
+    this.intvID = setInterval(() => {
+      intv += 2;
+      this.imgName = this.imgGroup[this.groupIndex][index++];
+      if (index >= this.imgGroup[this.groupIndex].length) {
+        clearInterval(this.intvID);
+        this.groupIndex++;
+      }
+    }, intv);
+  }
+
+  nextImage(imgIndex: number): string {
+    let baseName = "lock0000";
+    let imgName = baseName.substring(0, baseName.length - ((++imgIndex).toString().length)) + imgIndex + ".png";
+    return imgName;
+  }
+
+  ngOnDestroy() {
+    if (this.intvID) {
+      clearInterval(this.intvID);
+    }
   }
 
 }
